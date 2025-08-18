b718c7f05520dc32759b2c1b3e6268d6abc30de8
NeuralTrend LLC <contact@neuraltrend.org>
2025-08-17 23:34:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def backtest():
     # }
     results = {
         'ticker': ticker,
-        'final_value': float(eq_df['Equity'].to_numpy().flatten().astype(float).tolist()[-1]/),
+        'final_value': float(eq_df['Equity'].to_numpy().flatten().astype(float).tolist()[-1]),
         'profit_factor': float(eq_df['Equity'].to_numpy().flatten().astype(float).tolist()[-1])/initial_cash,
         'sharpe_ratio': sharpe_ratio,
         'equity_curve': equity_curve,
