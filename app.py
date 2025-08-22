c4ebacc7c0570578b87c4c06f8a357d7a56e3978
NeuralTrend LLC <contact@neuraltrend.org>
2025-08-22 13:34:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import yfinance as yf
 import pandas as pd  # FIXED
 import numpy as np
