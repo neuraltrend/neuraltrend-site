300b1a3480c007e6cc148d7cdf195a795b9cc5dc
NeuralTrend LLC <contact@neuraltrend.org>
2026-02-17 16:02:47 -0800

    Comment out main execution block in app.py
    Comment out the main execution block for app.

--- app.py
+++ app.py
@@ -551,5 +551,5 @@ def signals_summary():
     results = compute_signals_summary_cached(csv_version)
     return jsonify(results)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
