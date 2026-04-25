a2150cea1728b39b2e16fe3809419a2f30cd769d
NeuralTrend LLC <contact@neuraltrend.org>
2026-04-24 22:36:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ from functools import lru_cache
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
 # 🔐 REQUIRED FOR SESSIONS
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev-only-fallback")
 
