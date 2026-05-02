be4d46cb4e9241e3eef61e842384553c81b723c1
NeuralTrend LLC <contact@neuraltrend.org>
2026-05-01 22:55:21 -0700

    Update models.py

--- models.py
+++ models.py
@@ -8,10 +8,12 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     email = db.Column(db.String(255), unique=True, nullable=False)
-
     password_hash = db.Column(db.String(255), nullable=False)
 
     is_verified = db.Column(db.Boolean, default=False)
+
+    failed_attempts = db.Column(db.Integer, default=0)
+    locked_until = db.Column(db.DateTime, nullable=True)
 
     subscription_type = db.Column(
         db.String(50),
