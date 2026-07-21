adef322a8d661be603a5d2af4e8d2febab1c4492
NeuralTrend LLC <contact@neuraltrend.org>
2026-07-21 11:49:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -653,7 +653,7 @@ SUPPORTED_TICKERS = ['BTC-USD', 'ETH-USD', 'SOL-USD', 'XRP-USD', 'NVDA', 'AAPL',
                "ZIG-USD", "ZKJ-USD", "ZRX-USD"]
 
 TOP_FREE_TICKERS = {"BTC-USD", "ETH-USD", "SOL-USD", "XRP-USD"}
-ADMIN_ONLY_TICKERS = {"MU", "TSM", "ASML", "JNJ", "AVGO", "AMZN", "BRKB"}
+ADMIN_ONLY_TICKERS = {"MU", "TSM", "ASML", "JNJ", "AVGO", "AMZN", "BRKB", "NVDA1", "NVDA2", "NVDA4", "NVDA5", "NVDA6", "NVDA7", "NVDA8","NVDA9"}
 ALL_SUPPORTED_TICKERS = frozenset(
     str(ticker).strip().upper()
     for ticker in [*SUPPORTED_TICKERS, *ADMIN_ONLY_TICKERS]
